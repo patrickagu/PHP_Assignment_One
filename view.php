f162640471f199bdaa280dbda6b532a4a4ba1c6b
patrickagu <63365045+patrickagu@users.noreply.github.com>
2025-06-29 22:55:59 -0400

    Update view.php

--- view.php
+++ view.php
@@ -3,30 +3,18 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>CRUD in OOP PHP | Read</title>
-	<meta name="description" content="This week we will be using OOP PHP to create our CRUD application">
-	<meta name="robots" content="noindex, nofollow">
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" >
-  <link rel="stylesheet" href="./css/style.css">
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" ></script>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
+	<title>Content PHP | Read</title>
+	<meta name="description" content="Street Style Fashion App">
+        <link rel="stylesheet" href="./css/style.css">	
 </head>
 <body>
 	<header>
-    <nav class="navbar navbar-dark bg-primary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.php"><img src="./img/php-logo.png" alt="header logo"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="view.php">View</a></li>
+    <nav class="navbar">
+      <div class="nav-container">
+        <a class="navbar-brand" href="index.php"><img src="./img/company_logo.png" alt="header logo"></a>
+          <ul class="nav-menu">
+            <li class="nav-item"><a class="nav-link" aria-current="page" href="#">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="view.php">Content</a></li>
           </ul>
         </div>
       </div>
