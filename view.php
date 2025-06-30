e9088e8330ada2d2b543f16e0369063de565e5da
patrickagu <63365045+patrickagu@users.noreply.github.com>
2025-06-29 23:07:31 -0400

    Update view.php

--- view.php
+++ view.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Content PHP | Read</title>
+	<title>Data Content</title>
 	<meta name="description" content="Street Style Fashion App">
         <link rel="stylesheet" href="./css/style.css">	
 </head>
